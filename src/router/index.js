8635b09bce7c7ea97f58a114a27607328d0798f5
qyb225 <qyb225@gmail.com>
2018-06-30 22:17:49 +0800

    [router] add vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import CinemaManage from '../views/CinemaManage.vue'
+import ScheduleManage from '../views/ScheduleManage.vue'
 
 Vue.use(Router)
 
@@ -8,8 +9,14 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'cinema-manage',
+      component: CinemaManage
+    },
+
+    {
+      path: '/schedule/cinema/:cinema_id',
+      name: 'schedule-manage',
+      component: ScheduleManage
     }
   ]
 })
